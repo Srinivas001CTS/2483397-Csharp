8ba4f1bb05b8fa4849f99530578416deceb1fc76
Srinivas001CTS <Kanuparthi.Srinivasulu@cognizant.com>
2026-03-10 14:57:02 +0530

    Create _2_Increment.cs

---
+++ _2_Increment.cs
@@ -0,0 +1,21 @@
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //Post Increment
+        int a=5;
+        int b=a++; //use the value first, then increments
+        Console.WriteLine(b);
+        //Pre Increment
+        int x=5;
+        int y=++x;  //Increments first then use the value
+        Console.WriteLine(y);
+        
+        
+        
+        
+    }
+}
